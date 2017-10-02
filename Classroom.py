09a2129bf974669a1d86ff782d12a39cdc383be0
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-01 22:29:34 -0700

    Constructed more classes and added a few peices of data

---
+++ Classroom.py
@@ -0,0 +1,7 @@
+'''
+Classroom class that is used to create a roster for a class that is able to process student data based on the information given
+
+'''
+class Classroom:
+    def __init__(self,className,classDays,):
+        pass
---
+++ Student.py
@@ -0,0 +1,3 @@
+'''
+Student class. Contains all of the information to process all of the student information
+""
