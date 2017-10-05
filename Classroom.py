fc2d289f9f76a3e23d1a9c1d37e3a4a70321dc57
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-05 11:59:14 -0700

    rolled back all changes to refocus project into the right way

--- Classroom.py
+++ Classroom.py
@@ -1,13 +1,25 @@
+
+import Student.py
 '''
 Classroom class that is used to create a roster for a class that is able to process student data based on the information given
+this class must be created first for the sole reason that every student must be assigned a class before they can have things like attendence
+or assingments
 
+all of the students objects infor wiill be passed through here for the sole purpose of this being the processing module.
+my reasoning is that you cant have students without a classroom, so all of the student info will come from here and be executed via import and export
 '''
 class Classroom(object):
+    classroomcount = 0
     roster ={}
     def __init__(self,className,classDays):
         self.className = className
         self.classDays = classDays
 
+'''
+Sets the classroom name for a certin student
+'''
+    def _setClassName(className):
+        return self.className = className
 '''
 Gets all the student names and stores them in a dictionary(from when a student object is instantiated)
 '''
@@ -18,4 +30,14 @@ Gets all the student names and stores them in a dictionary(from when a student o
 Gets students grade from the info given in the student class and sets it into a roster
 '''
     def getStudentGrades():
-        psdd
+        pass
+'''
+Gets the total number of enrolled students in a certain class
+'''
+    def getNumOfStudents():
+        return classroomcount
+'''
+student counter for the class
+'''
+    def studentAdd():
+        pass
--- Student.py
+++ Student.py
@@ -1,3 +1,4 @@
+import Classroom.py
 
 '''
 Student class. Contains all of the information to process all of the student information
@@ -7,21 +8,5 @@ will use a function that uses a dictionary in order to be able to pass the infor
 
 for the grades a 0 denotes a zero on the assingment, while a null denotes an excused assingment
 '''
-class Student(Classroom):
-    classroomcount = 0
-    grades =list("")
-    def __init__(self,name, className,idNum):
-        Classroom.__init__(self, className)  # no name needed since
-        self.id = idNum
-        classroomcount += 1
-
-    def _setStudentGrades():
-        answer = raw_input ("\nWould you like to enter any grades for this student? Y for 'Yes', N for 'No' \n""
-        if answer =="Y":
-        for i in range (10):
-            print "Would you like to enter a grade for this student's assingment # %d ? \n Y for 'Yes', N for 'No'\n" % (i)
-
-            grades[i]= input("Please enter grade number %d " % (i))
-        return grades
-
-    def _getAbsences():
+class Student(object):
+    pass
