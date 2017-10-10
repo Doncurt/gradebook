9a7685ac9b75d1dd231930dfb19d2f1aaee44775
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-10 13:32:17 -0700

    finally got my mind in the project to finish it in an afternoon

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import Classroom
 import Student
+import Prompt
+'''
+Yes I call math courses Maths, like the British, get over it.
 
+This class is mostly for processing user intered information while encaplusating the rest of the functions and such
+'''
 print "\n***************************\n"
 print " Welcome to the Gradebook, Made by Donovan Adams \n"
-print " "
